18eb3176781d800e89870ff5ab7abc60c5ebcffd
StrikerXx798 <str1k4@yandex.ru>
2020-06-30 22:07:55 +0300

    Create Sign Up page

--- src/n1-main/m1-components/Header/Header.tsx
+++ src/n1-main/m1-components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+    import React from "react";
 import Navbar from "./Navbar/Navbar";
 import styles from './Header.module.css'
 import logo from '../../../assets/ing/logo.png'
--- src/n1-main/m1-components/SignUp/SignUp.tsx
+++ src/n1-main/m1-components/SignUp/SignUp.tsx
@@ -1,12 +1,60 @@
 import React from "react";
 import styles from './SignUp.module.css'
+import Input from "../common/Input/Input";
+import {NavLink} from "react-router-dom";
+import Button from "../common/Button/Button";
 
 const SignUp = () => {
+
+    const onSubmit = () => {
+        alert('Sign Up')
+    };
+
     return (
-        <div className={styles.pageWrapper}>
-            Sign Up
+        <div className={styles.signUpWrapper}>
+            <div className={styles.title}>
+                <h2>Sign Up</h2>
+            </div>
+            <SignUpForm onSubmit={onSubmit}/>
         </div>
     )
+};
+
+type PropsType = {
+    onSubmit: () => void
 }
+
+const SignUpForm = (props: PropsType) => {
+    return (
+        <>
+            <form className={styles.signUpForm}>
+                <div>
+                    <Input placeholderValue='Email'
+                           type='email'/>
+                </div>
+                <div>
+                    <Input placeholderValue='Password'
+                           type='password'/>
+                </div>
+                <div>
+                    <Input placeholderValue='Repeat Password'
+                           type='password'/>
+                </div>
+                <Button title='Register' onClick={props.onSubmit}/>
+            </form>
+            <div className={styles.signUp}>
+                <span className={styles.signIn}>
+                    You have account?
+                    <NavLink
+                        to={'/login'}
+                        activeClassName={styles.active}>
+                        Sign in
+                    </NavLink>
+                </span>
+            </div>
+        </>
+    );
+}
+
 
 export default SignUp
