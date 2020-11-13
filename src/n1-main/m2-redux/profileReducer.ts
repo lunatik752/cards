be6f8b92178ff4d8d5d1bc0ec1ff6db6b99a99bf
lunatik752 <whitemaxim752@gmail.com>
2020-11-13 16:16:33 +0300

    add reducers

---
+++ src/n1-main/m2-redux/profileReducer.ts
@@ -0,0 +1,23 @@
+type InitialStateType = typeof initialState
+
+const initialState = {
+}
+
+export const profileReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
+    switch (action.type) {
+
+        default :
+            return state;
+    }
+}
+
+// action types
+
+
+// action creators
+
+
+// Thunk
+
+
+
--- src/n1-main/m2-redux/store.ts
+++ src/n1-main/m2-redux/store.ts
@@ -1,9 +1,17 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunkMiddleware from "redux-thunk"
 import {registerReducer} from "./registerReducer";
+import {signInReducer} from "./signInReducer";
+import {recoverPassReducer} from "./recoverPassReducer";
+import {changePassReducer} from "./changePassReducer";
+import {profileReducer} from "./profileReducer";
 
 let rootReducers = combineReducers({
-    signUp: registerReducer
+    signUp: registerReducer,
+    signIn: signInReducer,
+    recoverPass: recoverPassReducer,
+    changePass: changePassReducer,
+    profile: profileReducer
 });
 
 export type AppRootStateType = ReturnType<typeof rootReducers>
