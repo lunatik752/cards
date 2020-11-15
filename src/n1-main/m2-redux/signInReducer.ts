254ae31ce41b3807b2c3b336fcddd57ddf666427
lunatik752 <whitemaxim752@gmail.com>
2020-11-15 15:02:31 +0300

    added cases and AC to signInReducer.ts

--- src/n1-main/m2-redux/signInReducer.ts
+++ src/n1-main/m2-redux/signInReducer.ts
@@ -1,23 +1,47 @@
-type InitialStateType = typeof initialState
+import { ResponseDataType } from "../m3-dal/auth-api"
 
-const initialState = {
+const SET_IS_LOGGED_IN = 'cards/signIn/SET-IS-LOGGED-IN'
+const SET_ERROR = 'cards/signIn/SET-ERROR'
+const SET_USER_DATA = 'cards/signIn/SET-USER-DATA'
+
+export type RequestErrorType = null | string
+
+
+type InitialStateType = {
+    isLoggedIn: boolean,
+    error: RequestErrorType,
+    userData: ResponseDataType | {}
 }
 
-export const signInReducer = (state: InitialStateType = initialState, action: any): InitialStateType => {
-    switch (action.type) {
+const initialState: InitialStateType = {
+    isLoggedIn: false,
+    error: null,
+    userData: {}
+}
 
+export const signInReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case SET_IS_LOGGED_IN:
+            return {...state, isLoggedIn: action.value};
+        case SET_ERROR:
+            return {...state, error: action.error};
+        case SET_USER_DATA:
+            return  {...state, userData: action.userData}
         default :
             return state;
     }
 }
 
 // action types
-
+type ActionsType = ReturnType<typeof setIsLoggedIn>
+    | ReturnType<typeof setError>
+    | ReturnType<typeof setUserData>
 
 // action creators
+export const setIsLoggedIn = (value: boolean) => ({type: SET_IS_LOGGED_IN, value} as const);
+export const setError = (error: RequestErrorType) => ({type: SET_ERROR, error} as const);
+export const setUserData = (userData: ResponseDataType) => ({type: SET_USER_DATA, userData} as const);
 
 
 // Thunk
-
-
 
