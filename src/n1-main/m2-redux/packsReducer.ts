29842103842be57cdd703e06fe48b8dbd380a1f8
lunatik752 <whitemaxim752@gmail.com>
2020-11-27 15:01:34 +0300

    added packsReducer.ts

---
+++ src/n1-main/m2-redux/packsReducer.ts
@@ -0,0 +1,64 @@
+import {Dispatch} from "redux";
+import {setError} from "./signInReducer";
+
+const SET_PACKS = 'cards/packs/SET-PACKS'
+
+export type PackType = {
+    _id: string;
+    user_id: string;
+    user_name: string;
+    private: boolean;
+
+    name: string;
+    path: string;
+    grade: number;
+    shots: number;
+
+    cardsCount: number;
+    deckCover: string;
+
+    type: string;
+    rating: number;
+    more_id: string;
+
+    created: string;
+    updated: string;
+}
+
+export type PacksStateType = {
+    packs: Array<PackType>;
+}
+
+const packsInitialState: PacksStateType = {
+    packs: [],
+};
+
+export const packsReducer = (state = packsInitialState, action: PacksActionsType): PacksStateType => {
+    switch (action.type) {
+        case SET_PACKS:
+            return {
+                ...state,
+                packs: action.packs,
+            }
+        default:
+            return state
+
+    }
+}
+
+// action types
+
+type PacksActionsType = ReturnType<typeof setPacks>
+
+export const setPacks = (packs: Array<PackType>) => {
+    return {type: SET_PACKS, packs} as const
+}
+
+
+export const getPacks = () => async (dispatch: Dispatch) => {
+    try {
+
+    } catch (err) {
+        console.log(err)
+    }
+}
