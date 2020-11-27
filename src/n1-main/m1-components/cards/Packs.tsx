3465dd3420902186de91f9e69e2ab945c37460e2
lunatik752 <whitemaxim752@gmail.com>
2020-11-27 14:24:19 +0300

    added table

--- src/n1-main/m1-components/cards/Packs.tsx
+++ src/n1-main/m1-components/cards/Packs.tsx
@@ -1,37 +1,49 @@
 import React from "react";
-import { Table } from 'antd';
+import { Table, Space } from 'antd';
 import 'antd/dist/antd.css';
+import Button from "../common/Button/Button";
 
 export const Packs = React.memo(() => {
     const dataSource = [
         {
             key: '1',
-            name: 'Mike',
-            age: 32,
-            address: '10 Downing Street',
-        },
-        {
-            key: '2',
-            name: 'John',
-            age: 42,
-            address: '10 Downing Street',
+            name: 'new pack',
+            cardsCount: 0,
+            updateDate: '------',
+
         },
     ];
     const columns = [
         {
-            title: 'Name',
+            title: 'Pack name ',
             dataIndex: 'name',
             key: 'name',
         },
         {
-            title: 'Age',
-            dataIndex: 'age',
-            key: 'age',
+            title: 'Cards count',
+            dataIndex: 'cardsCount',
+            key: 'cardsCount',
         },
         {
-            title: 'Address',
-            dataIndex: 'address',
-            key: 'address',
+            title: 'Update date',
+            dataIndex: 'updateDate',
+            key: 'updateDate',
+        },
+        {
+            title: 'Url',
+            dataIndex: 'url',
+            key: 'url',
+        },
+        {
+            title: <Button name={'Add pack'}/>,
+            dataIndex: 'addDeletePack',
+            key: 'addDeletePack',
+            url: '',
+            render: () => (
+                <Space size="middle">
+                    <Button name={'Delete pack'}/>
+                </Space>
+            ),
         },
     ];
     return (
